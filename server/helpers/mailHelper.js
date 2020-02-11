40372bd4e4c7d1fd0bcc087ffef001dd343eafaf
safaa <khadim.safaa@gmail.com>
2020-02-11 17:57:21 +0100

    send mail helper using nodemailer and sendgrid

---
+++ server/helpers/mailHelper.js
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
+
+var options = {
+  service: 'SendGrid',
+  auth: {
+    api_user: 'KHADIMSafaa',
+    api_key: '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'
+  }
+}
+
+var client = nodemailer.createTransport(sgTransport(options));
+
+
+export function sendmail(email){
+client.sendMail(email, function(err, info){
+    if (err ){
+      console.log(error);
+    }
+    else {
+      console.log('Message sent: ' + info.response);
+    }
+})};
